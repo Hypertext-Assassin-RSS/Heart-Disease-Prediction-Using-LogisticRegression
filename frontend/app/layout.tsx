43116afab73b2d1bd5d1f126be5664fadebdfd8e
Hypertext-Assassin-RSS <sanjayasenanayaka11@gmail.com>
2024-10-31 11:47:35 +0530

    fix overflow

--- frontend/app/layout.tsx
+++ frontend/app/layout.tsx
@@ -34,7 +34,7 @@ export default function RootLayout({
 }) {
   return (
     <html suppressHydrationWarning lang="en">
-      <head />
+      {/* <head /> */}
       <body
         className={clsx(
           "min-h-screen bg-background font-sans antialiased",
@@ -44,7 +44,7 @@ export default function RootLayout({
         <Providers themeProps={{ attribute: "class", defaultTheme: "dark" }}>
           <div className="relative flex flex-col h-screen">
             {/* <Navbar /> */}
-            <main className="container mx-auto max-w-7xl pt-16 px-6 flex-grow">
+            <main className="container mx-auto max-w-7xl pt-16 px-6 flex-grow overflow-hidden">
               {children}
             </main>
             <footer className="w-full flex items-center justify-center py-3">
